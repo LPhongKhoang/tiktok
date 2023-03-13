f033427e598f059cee8c4d761c946028346b7e41
long <longvv@hblab.vn>
2023-03-13 17:50:12 +0700

    [LongVV] Create header p1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { publicRoutes } from '@/routes';
-import { DefaultLayout } from '@/components/Layout'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          {publicRoutes.map((el, idx) => {
-            const Layout = el.layout || (el.layout === null ? Fragment : DefaultLayout);
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					{publicRoutes.map(
+						(el, idx) => {
+							const Layout =
+								el.layout ||
+								(el.layout === null
+									? Fragment
+									: DefaultLayout);
 
-            return <Route key={idx} path={el.path} element={<Layout><el.component /></Layout>} />
-          })}
-        </Routes>
-      </div>
-    </Router>
-  );
+							return (
+								<Route
+									key={idx}
+									path={el.path}
+									element={
+										<Layout>
+											<el.component />
+										</Layout>
+									}
+								/>
+							);
+						}
+					)}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,55 @@
-import { publicRoutes } from "../../../../routes";
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCircleXmark,
+	faMagnifyingGlass,
+	faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
 
 function Header() {
-  return <div>
-    <h2>Header</h2>
-    {publicRoutes.map((el, idx) => {
-      return <li key={idx}><a href={el.path}>{el.path.slice(1) || 'home'}</a></li>
-    })}
-  </div >;
+	return (
+		<header className={cx('wrapper')}>
+			<div className={cx('inner')}>
+				<div className={cx('logo')}>
+					<img
+						src={images.logo}
+						alt="TikTok"
+					/>
+				</div>
+				<div className={cx('search')}>
+					<input
+						placeholder="Search accounts and videos"
+						spellCheck="false"
+					/>
+					<button
+						className={cx('clear')}
+					>
+						<FontAwesomeIcon
+							icon={faCircleXmark}
+						/>
+					</button>
+					<FontAwesomeIcon
+						className={cx('loading')}
+						icon={faSpinner}
+					/>
+					<button
+						className={cx('search-btn')}
+					>
+						<FontAwesomeIcon
+							icon={faMagnifyingGlass}
+						/>
+					</button>
+				</div>
+				<div
+					className={cx('action')}
+				></div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
